b70e88d362fd2978fa232aa756a920faf56963e1
JULEEP <115367178+JULEEP@users.noreply.github.com>
2022-12-06 22:49:37 +0530

    Add files via upload

---
+++ Day_4- String_Program_Java.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Coldrink{
+	public static void main(String args[]){
+
+	String str = new String(7);
+	System.out.print("Enter the no of string");
+	Scanner sc = new Scanner(System.in);
+	str= sc.nextLine();
+
+	for(int i=0;i<7;i++){
+		str.nextLine();
+		//System.out.print("Strings are")
+	}
+	System.out.print("Strings are");
+	for(int i=0;i<7;i++){
+		System.out.print(str+" ");
+	}
+	
+}
+}
