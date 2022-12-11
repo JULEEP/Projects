7c0f5e57bb6d14deaa990cc2462c03bef0dd73e7
JULEEP <115367178+JULEEP@users.noreply.github.com>
2022-12-11 23:46:43 +0530

    Add files via upload

---
+++ Day_10_For_Each_Loop.java
@@ -0,0 +1,11 @@
+class Special{
+	public static void main(String args[]){
+
+	int a[] = {10,20,30,40,50};
+
+	for(int b : a){    //for each loop to print all element
+
+		System.out.print(b+" ");
+	}
+	}
+}
